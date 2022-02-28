a401f36c306df54d26258276e562f80cbf4ad43f
ZidaneSunesara <zidanesunesara786@gmail.com>
2022-02-28 10:28:10 +0530

    created our own range function using OOP concept for giving funtionality of a an iterator we create __next__ and __iter__ function so our program work as an iterator since the original python range() is also an iterator

---
+++ MyRange.py
@@ -0,0 +1,33 @@
+# we are going to create our own range() method
+# range() method is an iterator
+# iterable are not iterator BUT iterator are iterable
+
+from tracemalloc import start
+
+
+class MyRange:
+
+    def __init__(self, start, end) -> int:
+        self.value = start
+        self.end = end
+
+    def __iter__(self):
+        """
+        Creating __iter__() so our MyRange is iterable
+        for our MyRange to be iterator, __iter__() has to return iterator
+        i.e: it has to return an object that has __next__()
+        since we will create __next__() in this class we will return the same object(i.e self)
+        """
+        return self
+
+    def __next__(self):
+        if self.value > self.end:  # since range() exclude end value so our custom method will also exclude end value
+            raise StopIteration  # it value > end than we will raige StopIteration Error
+        current = self.value # storing current self.value
+        self.value += 1 # increment the value
+        return current
+    
+# as we can see below our own range() is been created
+for i in MyRange(1, 10):
+    print(i)
+
